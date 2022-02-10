9957568ad352a792918122dbc4f08ebcf43edda9
dara0799 <daraayufahira.daf@gmail.com>
2022-02-10 22:56:37 +0700

    dary's bookstore laravel 8

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::get('/test', function(Request $request){
+//     return 'YOU CAN USE YOUR API';
+// });
+
+Route::middleware('auth:api')->prefix('v1')->group(function(){
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
+    Route::apiResource('/authors', AuthorController::class);
+    Route::apiResource('/books', BookController::class);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// author/{author}
